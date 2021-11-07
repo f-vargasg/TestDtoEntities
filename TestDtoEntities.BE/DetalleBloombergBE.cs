ec76f74e3b46eb6ede4e673204aefcf7f499cb45
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2021-11-07 13:49:45 -0600

    Agregar archivos de proyecto.

---
+++ TestDtoEntities.BE/DetalleBloombergBE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDtoEntities.BE
+{
+    public class DetalleBloombergBE
+    {
+        string CodEncabloombergV { get; set; }
+        string CodFieldV { get; set; }
+        DateTime FecTransaccion { get; set; }
+        double ValBloombergN { get; set; }
+        int CodEstadoN { get; set; }
+        int CodRegistroN { get; set; }
+    }
+}
