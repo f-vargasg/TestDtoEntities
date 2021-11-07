20a8c742ce3cbaf45888d9b0222d6fb89639f546
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2021-11-07 13:56:01 -0600

    Estructura Inicial completa

--- TestDtoEntities.WinUI/FrmPrincipal.cs
+++ TestDtoEntities.WinUI/FrmPrincipal.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -15,6 +16,15 @@ namespace TestDtoEntities.WinUI
         public FrmPrincipal()
         {
             InitializeComponent();
+            InitMyComponents();
+
         }
+        private void InitMyComponents()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = ConfigurationManager.AppSettings["formCaption"];
+        }
+
+  
     }
 }
